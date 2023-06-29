6e2aa1b8f76bbf91eff542f4c7da720f20e8dfba
shulva <1329068252@qq.com>
2023-06-30 00:53:19 +0800

    python project 4 finished
    Former-commit-id: 57e12fa690d947cd95dd46952dfb27f8bd43c352 [formerly 1a9c620da27922a18c71667d6c13481a35f856ad]
    Former-commit-id: 3c40df899dc1339ef12d6c6b2d2db7a50b56e3a4

--- projects/AutomaticPython/4/4-11-2.py
+++ projects/AutomaticPython/4/4-11-2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import random
 """
 For this exercise, we’ll try doing an experiment. If you flip a coin 100 times 
 and write down an “H” for each heads and “T” for each tails, you’ll create 
@@ -8,8 +9,7 @@ tail results like “H T H T H H T H T T,” which looks random (to humans),
 but isn’t mathematically random. A human will almost never write down 
 a streak of six heads or six tails in a row, even though it is highly likely 
 to happen in truly random coin flips. Humans are predictably bad at 
-being random.
- Write a program to find out how often a streak of six heads or a streak 
+being random. Write a program to find out how often a streak of six heads or a streak 
 of six tails comes up in a randomly generated list of heads and tails. Your 
 program breaks up the experiment into two parts: the first part generates 
 a list of randomly selected 'heads' and 'tails' values, and the second part 
@@ -19,7 +19,6 @@ flips contains a streak of six heads or tails in a row. As a hint, the function
 call random.randint(0, 1) will return a 0 value 50% of the time and a 1 value 
 the other 50% of the time.
 """
-import random
 numberOfStreaks = 0
 for experimentNumber in range(10000):
     # Code that creates a list of 100 'heads' or 'tails' values.
@@ -27,15 +26,15 @@ for experimentNumber in range(10000):
     count=0
     for i in range(100):
         list.append(random.randint(0,1))
+        
     # Code that checks if there is a streak of 6 heads or tails in a row.
     for i in range(99):
         if list[i]==list[i+1]:
             count+=1
+            if count%5==0:# 0%5 also equals to zero,so if you put the if out the big if , it will be wrong
+                numberOfStreaks+=1
+                break
         else:
             count=0
-
-        if count%5==0:
-            numberOfStreaks+=1
-            break
 
 print('Chance of streak: %s%%' % (numberOfStreaks / 100))
