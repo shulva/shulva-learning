48ee4feca27957e50c387d68cb87323b8c152c2f
shulva <1329068252@qq.com>
2023-08-09 10:33:49 +0800

    aa:
    Former-commit-id: e366f58353a8e2d3bd011397bf90eaf415c8cf3d [formerly bafac8a530aef9801bd9b2c8a86c8088b0958f68]
    Former-commit-id: 0a1ed5c96f4bd6d681ba88a2898dca93b5236347

---
+++ projects/AutomaticPython/12/12-11-1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# pdf-308 book-210
+
+from selenium import webdriver
+browser = webdriver.Edge()
+browser.get('https://mail.qq.com/cgi-bin/frame_html?sid=5jo7LdUsGi3-djld&from=')
