fddde1e58160d1543a5f9c7195a67b770e500b26
shulva <1329068252@qq.com>
2023-08-09 20:15:27 +0800

    py12
    Former-commit-id: 2b3fc05012c91221d371182de37c8a98246b1df8 [formerly 853913eb660ac1061f229c7e0960d4dc18e93194]
    Former-commit-id: 3bda9f2557f0420dc634c20344eec23ae743b386

--- projects/AutomaticPython/12/12-11-1.py
+++ projects/AutomaticPython/12/12-11-1.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # pdf-308 book-210
-
+# use selenium and webdriver-manager
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-browser = webdriver.Firefox()
-browser.get('https://mail.qq.com/cgi-bin/frame_html?sid=5jo7LdUsGi3-djld&from=')
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach',True)
+
+browser = webdriver.Chrome(options=options)
+browser.get('https://mail.qq.com')
+
+password = browser.find_element(By.CSS_SELECTOR,'a#switcher.plogin.link')
+print(password.text)
+password.click()
